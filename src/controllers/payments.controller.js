eae9861bd6f3b576db3109e62adcd2580aef01f7
Andrew <krakenmoster29@gmail.com>
2026-02-01 22:30:09 +0000

    Make create payment controller async for sqlite

--- src/controllers/payments.controller.js
+++ src/controllers/payments.controller.js
@@ -1,23 +1,20 @@
 const { createPayment } = require("../services/payments.service");
 const { savePayment } = require("../services/payments.store");
 
-function createPaymentController(req, res) {
+async function createPaymentController(req, res) {
   try {
-    // ожидаем body: { amount, currency, userId }
     const payment = createPayment(req.body ?? {});
-
-    // сохраняем платёж в in-memory store
-    savePayment(payment);
+    await savePayment(payment);
 
     return res.status(201).json({
       ok: true,
-      payment
+      payment,
     });
   } catch (err) {
     return res.status(400).json({
       ok: false,
       error: err.message,
-      received: req.body
+      received: req.body,
     });
   }
 }
